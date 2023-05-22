c649068fc03f7728d7834d91a29163d4fb18782e
Manika Sharma <49314189+manika29@users.noreply.github.com>
2023-05-22 13:42:20 +0530

    Add files via upload

---
+++ studybud/base/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from .models import Room
+from django.contrib.auth.models import User
+
+# class MyUserCreationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ['name', 'username', 'email', 'password1', 'password2']
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude=['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
